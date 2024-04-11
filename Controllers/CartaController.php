70295858b631b5de7229a60a0143dfe20bdd2be8
luciarol <luciarol87@gmail.com>
2024-04-11 12:10:38 +0200

    mostrar carta terminado

--- Controllers/CartaController.php
+++ Controllers/CartaController.php
@@ -10,7 +10,23 @@ class CartaController {
 
         // Construct the image URL based on the card's suit and number
         $imagen = "./img/{$carta->getPalo()}_{$carta->getNumero()}.jpg";
-        echo "<img src='$imagen' alt=''>";
+        echo "<img src='$imagen' alt=''><hr>";
+        
+        $cartasRestantes = [];
+        foreach (Carta::PALOS as $palo) {
+            foreach (Carta::CARTAS as $numero => $valor) {
+                if ($numero !== $carta->getNumero() || $palo !== $carta->getPalo()) {
+                    $cartasRestantes[] = new Carta($numero, $palo);
+                }
+            }
+        }
+
+        // Display images of the remaining cards
+        foreach ($cartasRestantes as $cartaRestante) {
+            $imagenRestante = "./img/{$cartaRestante->getPalo()}_{$cartaRestante->getNumero()}.jpg";
+            echo "<img src='$imagenRestante' alt=''>";
+        }
+
 
         // Include the view to display the card image
         require_once 'Views/Menu/mostrarCarta.php';
