9949b9332c47b8d444ce8bfd6ce898b7a2831272
luciarol <luciarol87@gmail.com>
2024-04-11 11:40:37 +0200

    baraja barajada

--- Controllers/BarajaController.php
+++ Controllers/BarajaController.php
@@ -6,6 +6,7 @@ use Models\Baraja;
 Class BarajaController {
 
     function mostrarBaraja(){
+        require_once 'Views\Menu\mostrarBaraja.php';
         // Crea una instancia de la baraja para obtener las rutas de las imágenes
         $baraja = new Baraja();
         // Baraja el mazo de cartas
@@ -18,13 +19,27 @@ Class BarajaController {
             // Muestra la imagen
             echo "<img src='$imagen' alt=''>";
         }
+        
     
         // Imprime un título indicando que estas son las imágenes de la baraja
         echo "<h2>Estas son las imágenes de la baraja</h2>";
     }
 
     function barajar(){
+        
         require_once 'Views\Menu\mazoBarajado.php';
+        $baraja = new Baraja();
+        // Baraja el mazo de cartas
+        $baraja->barajarMazo();
+        // Obtiene la baraja de rutas de imágenes
+        $imagenes = $baraja->getBaraja();
+    
+        // Itera sobre la lista de imágenes y las muestra en la página
+        foreach ($imagenes as $imagen) {
+            // Muestra la imagen
+            echo "<img src='$imagen' alt=''>";
+        }
+        
         echo "<h2>Esta es la opción BARAJAR EL MAZO, que muestra el mazo completo barajado</h2>";
     }
 
