55075a9db74591aed5ebc7b9262431c08c50ff65
luciarol <luciarol87@gmail.com>
2024-04-17 13:30:59 +0200

    añadidas baraja barajada y baraja con carta extraídas en cada opción.

--- Controllers/CartaController.php
+++ Controllers/CartaController.php
@@ -4,9 +4,23 @@ use Controllers\FrontController;
 
 use Models\Carta;
 use Models\Baraja;
+use Lib\Pages;
+
 
 class CartaController {
     function mostrarCarta() {
+        $pagina = new Pages;
+
+        /* Llamar al método render antes de imprimir las imágenes:
+        esto es lo que sustituye al require_once cuando se usa el Pages.php 
+        require_once 'Views\Menu\mostrarbaraja.php';*/
+        
+        //$pagina->render ('Menu/mostrarBaraja', ['imagen'=> $imagen]);
+        $pagina->render('Menu/mostrarBaraja');
+        
+        echo "<h2>Esta es la opción Mostrar CARTA, que muestra la carta que se extrae al azar y la baraja sin esa carta</h2>";
+        
+        
         $carta = Carta::generarCartaAleatoria();
 
         // muestra una carta
@@ -31,41 +45,121 @@ class CartaController {
 
         require_once 'Views/Menu/mostrarCarta.php';
 
-        echo "<h2>Esta es la opción Mostrar CARTA, que muestra la carta que se extrae al azar y la baraja sin esa carta</h2>";
     }
 
     function sacarUltimaCarta() {
-
-        $baraja = new Baraja();
-        /* $baraja->barajarMazo(); */
-        $ultimaCarta = $baraja->extraerCarta();
-
-        echo "<img src='$ultimaCarta' alt=''>";
-
-        require_once 'Views/Menu/sacarUltimaCarta.php';
+        $pagina = new Pages;
+    
+        // Renderizar la vista mostrarBaraja
+        $pagina->render('Menu/mostrarBaraja');
+        
         echo "<h2>Esta es la opción SACAR ÚLTIMA CARTA DEL MAZO. El mazo está sin barajar</h2>";
+    
+        $baraja = new Baraja();
+        // No es necesario barajar el mazo si solo queremos sacar la última carta
+        $baraja->barajarMazo(); 
+        
+        $imagenes = $baraja->getBaraja();
+        // Mostrar la baraja completa
+        echo "<h3>Baraja completa:</h3>";
+        foreach ($imagenes as $imagen) {
+            echo "<img src='$imagen' alt=''>";
+        }
+
+        echo "<p>Última Carta</p><hr>";
+
+        $ultimaCarta = $baraja->extraerCarta();
+        
+
+        
+        // Mostrar la última carta extraída
+        echo "<img src='$ultimaCarta' alt=''>";
+    
+        // Obtener las imágenes de todas las cartas restantes en la baraja
+        $cartasRestantes = $baraja->getBaraja();
+    
+        // Mostrar todas las cartas restantes en la baraja
+        echo "<h3>Cartas restantes en el mazo:</h3>";
+        foreach ($cartasRestantes as $carta) {
+            echo "<img src='$carta' alt=''>";
+        }
+    
+        require_once 'Views/Menu/sacarUltimaCarta.php';
     }
+    
 
     function tresCartas() {
+
+        $pagina = new Pages;
+
+        /* Llamar al método render antes de imprimir las imágenes:
+        esto es lo que sustituye al require_once cuando se usa el Pages.php 
+        require_once 'Views\Menu\mostrarbaraja.php';*/
         
+        //$pagina->render ('Menu/mostrarBaraja', ['imagen'=> $imagen]);
+        $pagina->render('Menu/mostrarBaraja');
+        
+        echo "<h2>Esta es la opción REPARTIR DIEZ CARTAS A UN JUGADOR</h2>";
+
         $baraja = new Baraja();
         $baraja->barajarMazo();
         $imagenes = $baraja->getBaraja();
-    
-        // Mostrar las imágenes de las tres primeras cartas
-        for ($i = 0; $i < 3; $i++) {
-            echo "<img src='{$imagenes[$i]}' alt=''>";
+
+            // Mostrar la baraja completa
+        echo "<h3>Baraja completa:</h3>";
+        foreach ($imagenes as $imagen) {
+            echo "<img src='$imagen' alt=''>";
         }
 
-        require_once 'Views/Menu/tresCartas.php';
-        echo "<h2>Esta es la opción REPARTIR TRES CARTAS A UN JUGADOR</h2>";
+        echo "<p>3 cartas</p><hr>";
+    
+        // Mostrar las imágenes de las diez cartas y guardar los números de las cartas
+        $numerosCartas = [];
+        for ($i = 0; $i < 3; $i++) {
+            $rutaCarta = $imagenes[$i];
+            $numeroCarta = basename($rutaCarta, '.jpg'); // Obtenemos el número de la carta desde la ruta de la imagen
+            $numerosCartas[] = $numeroCarta; // Guardamos el número de la carta
+            echo "<img src='$rutaCarta' alt=''>";
+        }
+    
+        // Calcula el total de puntos de las 10 cartas
+        $totalPuntos = Baraja::calcularTotal($numerosCartas);
+        echo "<p>Total de puntos de las 3 cartas: $totalPuntos</p>";
+    
+        // Mostrar el resto de las cartas del mazo
+        echo "<h3>Cartas restantes en el mazo:</h3>";
+        for ($i = 3; $i < count($imagenes); $i++) {
+            $rutaCarta = $imagenes[$i];
+            echo "<img src='$rutaCarta' alt=''>";
+        }
+    
+        require_once 'Views/Menu/diezCartas.php';
     }
 
     function diezCartas() {
+
+        $pagina = new Pages;
+
+        /* Llamar al método render antes de imprimir las imágenes:
+        esto es lo que sustituye al require_once cuando se usa el Pages.php 
+        require_once 'Views\Menu\mostrarbaraja.php';*/
         
+        //$pagina->render ('Menu/mostrarBaraja', ['imagen'=> $imagen]);
+        $pagina->render('Menu/mostrarBaraja');
+        
+        echo "<h2>Esta es la opción REPARTIR DIEZ CARTAS A UN JUGADOR</h2>";
+
         $baraja = new Baraja();
         $baraja->barajarMazo();
         $imagenes = $baraja->getBaraja();
+
+        // Mostrar la baraja completa
+        echo "<h3>Baraja completa:</h3>";
+        foreach ($imagenes as $imagen) {
+            echo "<img src='$imagen' alt=''>";
+        }
+
+        echo "<p>10 cartas</p><hr>";
     
         // Mostrar las imágenes de las diez cartas y guardar los números de las cartas
         $numerosCartas = [];
@@ -75,45 +169,63 @@ class CartaController {
             $numerosCartas[] = $numeroCarta; // Guardamos el número de la carta
             echo "<img src='$rutaCarta' alt=''>";
         }
-
+    
         // Calcula el total de puntos de las 10 cartas
         $totalPuntos = Baraja::calcularTotal($numerosCartas);
         echo "<p>Total de puntos de las 10 cartas: $totalPuntos</p>";
-
-        require_once 'Views/Menu/diezCartas.php';
-        echo "<h2>Esta es la opción REPARTIR DIEZ CARTAS A UN JUGADOR</h2>";
     
+        // Mostrar el resto de las cartas del mazo
+        echo "<h3>Cartas restantes en el mazo:</h3>";
+        for ($i = 10; $i < count($imagenes); $i++) {
+            $rutaCarta = $imagenes[$i];
+            echo "<img src='$rutaCarta' alt=''>";
+        }
+    
+        require_once 'Views/Menu/diezCartas.php';
     }
+    
 
     function variosJugadores() {
+        $pagina = new Pages;
+    
+        // Renderizar la vista mostrarBaraja
+        $pagina->render('Menu/mostrarBaraja');
+        
+        echo "<h3>Baraja completa:</h3>";
+    
         $num_jugadores = 2;
         $baraja = new Baraja();
         $baraja->barajarMazo();
         $imagenes = $baraja->getBaraja();
-        
-        // Dividir el mazo en dos partes, una para cada jugador
+    
+        // Mostrar la baraja completa
+        foreach ($imagenes as $imagen) {
+            echo "<img src='$imagen' alt=''>";
+        }
+    
+        // Dividir el mazo en partes iguales para cada jugador
         $mitadMazo = count($imagenes) / $num_jugadores;
-        $jugador1Cartas = array_slice($imagenes, 0, $mitadMazo);
-        $jugador2Cartas = array_slice($imagenes, $mitadMazo);
-    
-        // Mostrar las imágenes de las tres primeras cartas para el jugador 1
-        echo "<h2>Jugador 1</h2>";
-        for ($i = 0; $i < 3; $i++) {
-            echo "<img src='{$jugador1Cartas[$i]}' alt=''>";
+        $jugadoresCartas = [];
+        for ($i = 0; $i < $num_jugadores; $i++) {
+            $jugadoresCartas[] = array_slice($imagenes, $i * $mitadMazo, $mitadMazo);
         }
     
-        // Mostrar las imágenes de las tres primeras cartas para el jugador 2
-        echo "<h2>Jugador 2</h2>";
-        for ($i = 0; $i < 3; $i++) {
-            echo "<img src='{$jugador2Cartas[$i]}' alt=''>";
+        // Mostrar las imágenes de las primeras tres cartas para cada jugador
+        for ($j = 0; $j < $num_jugadores; $j++) {
+            echo "<h2>Jugador " . ($j + 1) . "</h2>";
+            for ($i = 0; $i < 3; $i++) {
+                echo "<img src='{$jugadoresCartas[$j][$i]}' alt=''>";
+            }
+        }
+    
+        // Mostrar las imágenes restantes de la baraja
+        echo "<h3>Cartas restantes en el mazo:</h3>";
+        $cartainicio = $num_jugadores * 3; 
+        for ($i = $cartainicio; $i < count($imagenes); $i++) {
+            $rutaCarta = $imagenes[$i];
+            echo "<img src='$rutaCarta' alt=''>";
         }
     
     }
     
-    
-
-    function partida(){
-        require_once 'Views/Menu/partida.php';
-        echo "<h2>la partida</h2>";
-    }
 }
